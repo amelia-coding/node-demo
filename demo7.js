3db1b035695d14eb50332b8997124313f52c9a71
amelia-coding <sweety_juaner@163.com>
2021-09-15 10:42:33 +0800

    Update demo7.js

--- demo7.js
+++ demo7.js
@@ -2,13 +2,18 @@
  config.alinode = {
   server: 'wss://agentserver.node.aliyun.com:8080',
   appid: '86946',
-  secret: '1fb502bbe0013d48b37751a941963538d5118aeb',
+  secret: 'your secret',
 }
 
 exports.alinode = {
   enable: true,
   package: 'egg-alinode',
 }
-// sudo npm i egg-alinode
-// "prestart":"nodeinstall --install-alinode 4.6.1",
-// sudo npm start
+
+// 脚本配置
+"prestart":"nodeinstall --install-alinode 4.6.1",
+
+//控制台执行
+sudo npm i egg-alinode
+
+sudo npm start
