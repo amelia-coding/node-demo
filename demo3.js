13cf15fe9282c3fc318fb8521d8fd07767acf4f2
dujuan <dujuan@bixin.cn>
2021-09-15 11:01:43 +0800

    fix: io

--- demo3.js
+++ demo3.js
@@ -1,3 +1,10 @@
 const os = require('os')
 
-console.log("os",os)
+//Returns an array of objects containing information about each logical CPU core.
+console.log("cpus:",os.cpus())
+
+//Returns an array containing the 1, 5, and 15 minute load averages.
+console.log("loadavg:",os.loadavg())
+
+//Returns the amount of free system memory in bytes as an integer.
+console.log("freemem:",os.freemem())
